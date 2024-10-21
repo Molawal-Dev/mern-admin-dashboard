480581eebe2dfef91e059650e5d27a9c58380690
Mohlawal-Dev <muhammedlawal669@gmail.com>
2024-10-21 12:24:13 +0100

    second commit

---
+++ client/src/main.jsx
@@ -0,0 +1,22 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+import { configureStore } from "@reduxjs/toolkit";
+import globalReducer from "./state";
+import { Provider } from "react-redux";
+
+const store = configureStore({
+  reducer: {
+    global: globalReducer,
+  },
+});
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+)
--- server/index.js
+++ server/index.js
@@ -3,7 +3,14 @@ import mongoose from "mongoose";
 import cors from "cors";
 import helmet from "helmet"
 import morgan from "morgan";
+import dotenv from "dotenv";
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import salesRoutes from "./routes/sales.js";
+import managementRoutes from "./routes/management.js";
 
+
+dotenv.config();
 const app = express();
 app.use(express.json())
 app.use(helmet())
@@ -16,3 +23,11 @@ app.use("/client", clientRoutes)
 app.use("/general", generalRoutes)
 app.use("/management", managementRoutes)
 app.use("/sales", salesRoutes)
+
+
+
+//MONGOOSE
+const PORT = process.env.PORT || 8000;
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));  
+}).catch((error) => console.log(`${error} did not connect`));
