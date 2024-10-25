0ae7431f7a914bc8ba760ace714bbdd47646a00c
Mohlawal-Dev <muhammedlawal669@gmail.com>
2024-10-25 10:05:59 +0100

    updating

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -1,17 +1,27 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Box, useMediaQuery } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Navbar from "../../components/Navbar";
 import Sidebar from "../../components/Sidebar";
+import { useGetUserQuery } from "../../state/api";
 
 
 const Layout = () => {
   const isNonMobile = useMediaQuery("(min-width: 600px)");
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  //userId defined as a global state.
+  const userId = useSelector((state) => state.global.userId);
+  //getting the data from RTK query.
+  const { data } = useGetUserQuery(userId);
+  console.log(data)
+
+
+
   return (
     <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
       <Sidebar
+        user={data || {}}
         isNonMobile={isNonMobile}
         drawerWidth="250px"
         isSidebarOpen={isSidebarOpen}
@@ -19,6 +29,7 @@ const Layout = () => {
       />
       <Box flexGrow={1}>
         <Navbar
+          user={data || {}}
           isSidebarOpen={isSidebarOpen}
           setIsSidebarOpen={setIsSidebarOpen}
         />
--- server/index.js
+++ server/index.js
@@ -9,6 +9,21 @@ import generalRoutes from "./routes/general.js";
 import salesRoutes from "./routes/sales.js";
 import managementRoutes from "./routes/management.js";
 
+// Mock data and models imports
+import User from "./models/User.js";
+import Product from "./models/Product.js";
+import ProductStat from "./models/ProductStat.js";
+import Transaction from "./models/Transaction.js";
+import OverallStat from "./models/OverallStat.js";
+import AffiliateStat from "./models/AffiliateStat.js";
+import {
+  dataUser,
+  dataProduct,
+  dataProductStat,
+  dataTransaction,
+  dataOverallStat,
+  dataAffiliateStat,
+} from "./data/index.js";
 
 dotenv.config();
 const app = express();
@@ -29,5 +44,14 @@ app.use("/sales", salesRoutes)
 //MONGOOSE
 const PORT = process.env.PORT || 8000;
 mongoose.connect(process.env.MONGO_URL).then(() => {
-    app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));  
+    app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`)); 
+    
+    
+  //User.insertMany(dataUser);
+  //Product.insertMany(dataProduct);
+  //ProductStat.insertMany(dataProductStat);
+  //Transaction.insertMany(dataTransaction);
+  //AffiliateStat.insertMany(dataAffiliateStat);
+  //OverallStat.insertMany(dataOverallStat);
+
 }).catch((error) => console.log(`${error} did not connect`));
